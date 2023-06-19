59d8f2778484b9891aba52b11c7c015fe4ad03d5
James Treap <jtreap@blackberry.com>
2023-06-19 11:26:52 -0400

    Added bar graph to provide monthly overview of expenses.
    Dynamically renders using a stateful list, and provides expense amount per month.

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import './Expenses.css';
+import { useState } from 'react';
+import ExpensesFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
+import ExpensesChart from './ExpensesChart';
+import Card from '../UI/Card';
+
+function Expenses(props) {
+    const [theYear, setTheYear] = useState('2023');
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === theYear;
+    });
+
+    const filterHandler = selYear => {
+        setTheYear(selYear);
+    };
+
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter
+                    selected={theYear}
+                    onChangeFilter={filterHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpenseList items={filteredExpenses} />
+            </Card>
+        </div>
+    );
+}
+
+export default Expenses;
